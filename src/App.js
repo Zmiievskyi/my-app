e300da8b9cc312547b49174bc87cb06a7b6a0980
obayasiky <obayasiky@gmail.com>
2024-06-21 16:42:52 +0100

    product test 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>App.js</code> Hello Dude
+           <code>App.js</code> Hello Dude
         </p>
         <a
           className="App-link"
